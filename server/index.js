e9e346704d2525a4d5d68980532e78854cb3e0b2
sanshigo345 <bugrasshgercan@gmail.com>
2023-01-23 17:44:57 +0300

    initial commit

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "04d404ad18d4e986af89b44a2a99eade34c4ad1a2af1161b7a44c2b07831063807b1ea7b272e4b077b22a8ff4fb0136fa796a87a7697772db568ded17e85bfea3c": 100,
+  "040133d99aeeb75dedc605e74fcf2ec31c2c9ae80bf428885ec20aa2be6151a9c5c268b941027b69bdae45df85fc2524ecbe6101188ae6d21201e9244b91a76a13": 50,
+  "0405b67c42dc463835f7895b1a5218e223c61564b08faadcd17105cf5280cf116022e18c82d2200dcb56fdbccae0d84eeac4a1e1a09ca350b06a0267dd0ac81f87": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,6 +19,10 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+  // TODO: get a signature from the client-side application
+  // recover the public address from the signature
+  // commit try
+
   const { sender, recipient, amount } = req.body;
 
   setInitialBalance(sender);
